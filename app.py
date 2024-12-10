e7ba9652304e8b2ef5b8108bd46caf1ed45dff00
Michał Strzelczyk <817457+panmichal@users.noreply.github.com>
2024-12-10 18:21:32 +0100

    Show both maps in one row

--- app.py
+++ app.py
@@ -19,9 +19,12 @@ app.layout = [
             dcc.Input(id="gtfs-file", type="text", placeholder="Enter GTFS source"),
             html.Button('Submit', id='submit-geometry', n_clicks=0)
         ])), justify="center"),
-        dbc.Row(dbc.Col(html.Div([dcc.Graph(id="graph", style={"height": "100%"})])), justify="center"),
-        dbc.Row(dbc.Col(html.Div([dcc.Graph(id="graph-scatter", style={"height": "100%"})])), justify="center")
-        ])
+        dbc.Row(
+            [
+                dbc.Col(html.Div([dcc.Graph(id="graph", style={"height": "100%"})])), 
+                dbc.Col(html.Div([dcc.Graph(id="graph-scatter", style={"height": "100%"})]))], 
+                justify="center"
+        )])
 ]
 
 @app.callback(
@@ -61,49 +64,6 @@ def display_chloropleth(n_clicks, geojson_file, gtfs_dir):
                                )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
-
-
-
-    # #iterat
-    # # gtfs_gdf = gpd.sjoin(gtfs_gdf, gdf, how="left")
-    # # gtfs_gdf = gtfs_gdf.groupby('id').size().reset_index(name='count')
-    # # gtfs_gdf = gtfs_gdf.rename(columns={'id': 'id_area'})
-    # # gdf = gdf.merge(gtfs_gdf, on='id_area', how='left')
-    # # gdf['count'] = gdf['count'].fillna(0)
-    # # gdf['area'] = gdf['geometry'].area
-    # # gdf['stops_density'] = gdf['count'] / gdf['area']
-    # # fig = px.choropleth_mapbox(gdf, geojson=gdf.geometry, locations=gdf.index, color="stops_density",
-    # #                        hover_data=["id_area", "count", "area", "stops_density"],
-    # #                        mapbox_style="carto-positron",
-    # #                        zoom=11, center = {"lat": 51.11046075118283, "lon": 17.03258604846201},
-    # #                        opacity=0.5,
-    # #                        labels={'stops_density':'stops density'})
-    # # Center of Wrocław
-    # point = (51.11046075118283, 17.03258604846201)
-    # fig = go.Figure(px.scatter_mapbox(gdf, lat=[point[0]], lon=[point[1]]).update_layout(
-    #     mapbox={
-    #         "style": "open-street-map",
-    #         "zoom": 14,
-    #         "layers": [
-    #             {
-    #                 "source": json.loads(gdf.geometry.to_json()),
-    #                 "below": "traces",
-    #                 "type": "line",
-    #                 "color": "red",
-    #                 "line": {"width": 2.5},
-    #             },
-    #                             {
-    #                 "source": json.loads(gtfs_gdf.geometry.to_json()),
-    #                 "below": "traces",
-    #                 "type": "circle",
-    #                 "color": "blue",
-    #                 "circle": {"radius": 2.5},
-    #             }
-    #         ],
-    #     },
-    #     margin={"l": 0, "r": 0, "t": 0, "b": 0},
-    # ))
-    # return fig
 
 @app.callback(
     Output("graph-scatter", "figure"), 
