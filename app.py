9c23209caac00068d2034c8f221132815903f8c4
Michał Strzelczyk <817457+panmichal@users.noreply.github.com>
2024-12-11 12:22:00 +0100

    Use single callback for both graphs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import packages
-from dash import Dash, html, State, dash_table, dcc, callback, Output, Input
+from dash import Dash, State, html, dcc, Output, Input
 import dash_bootstrap_components as dbc
 import pandas as pd
 import geopandas as gpd
@@ -8,27 +8,73 @@ import plotly.graph_objs as go
 import json
 
 
-app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-# App layout
-app.layout = [
-    html.Div([
-        dbc.Row(dbc.Col(html.H4('Stops density by area')), justify="center"), 
-        dbc.Row(dbc.Col(html.Div(className='row', children = [
-            dcc.Input(id="geometry-file", type="text", placeholder="Enter geometry (Geojson, Shapefile etc.) source"),
-            dcc.Input(id="gtfs-file", type="text", placeholder="Enter GTFS source"),
-            html.Button('Submit', id='submit-geometry', n_clicks=0)
-        ])), justify="center"),
-        dbc.Row(
-            [
-                dbc.Col(html.Div([dcc.Graph(id="graph", style={"height": "100%"})])), 
-                dbc.Col(html.Div([dcc.Graph(id="graph-scatter", style={"height": "100%"})]))], 
-                justify="center"
-        )])
-]
+app = Dash(__name__, 
+    meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+)
+app.layout = html.Div(
+    children=[
+        html.Div(
+            className="row",
+            children=[
+                # Column for user controls
+                html.Div(
+                    className="four columns div-user-controls",
+                    children=[
+                        html.H2("Public transport stops density"),
+                        html.P(
+                            """Load geometry and GTFS package and visualize the public transport stops density
+                            in each polygon.
+                            """
+                        ),
+                        html.Div(
+                            className="div-for-input",
+                            children=[
+                                dcc.Input(id="geometry-file", type="text", placeholder="Enter geometry (Geojson, Shapefile etc.) source")
+                            ],
+                        ),
+                        # Change to side-by-side for mobile layout
+                        html.Div(
+                            className="row",
+                            children=[
+                                html.Div(
+                                    className="div-for-input",
+                                    children=[
+                                        dcc.Input(id="gtfs-file", type="text", placeholder="Enter GTFS source"),
+                                    ],
+                                ),
+                            ],
+                        ),
+                        html.Div(
+                            className="row",
+                            children=[
+                                html.Div(
+                                    className="div-for-input",
+                                    children=[
+                                        html.Button('Submit', id='submit-geometry', n_clicks=0),
+                                    ],
+                                ),
+                            ],
+                        ),
+                        html.P(id="total-stops"),
+                        html.P(id="total-area"),
+                    ],
+                ),
+                # Column for app graphs and plots
+                html.Div(
+                    className="eight columns div-for-charts bg-grey",
+                    children=[
+                        dcc.Graph(id="graph", style={"height": "100%"}),
+                        dcc.Graph(id="graph-scatter", style={"height": "100%"}),
+                    ],
+                ),
+            ],
+        )
+    ]
+)
 
 @app.callback(
     Output("graph", "figure"), 
+    Output("graph-scatter", "figure"), 
     Input("submit-geometry", "n_clicks"),
     State("geometry-file", "value"),
     State("gtfs-file", "value"))
@@ -55,54 +101,43 @@ def display_chloropleth(n_clicks, geojson_file, gtfs_dir):
     gdf['count'] = gdf['count'].fillna(0)
     gdf['stops_density'] = gdf['area'] / gdf['count']
  
-    fig = px.choropleth_mapbox(gdf, geojson=gdf.geometry, locations=gdf.index, color="stops_density",
+    choropleth_fig = px.choropleth_mapbox(gdf, geojson=gdf.geometry, locations=gdf.index, color="stops_density",
                                hover_data=["NAZWAOSIED", "count", "area", "stops_density"],
                                mapbox_style="carto-positron",
                                zoom=11, center = {"lat": 51.11046075118283, "lon": 17.03258604846201},
                                opacity=0.5,
                                color_continuous_scale=px.colors.sequential.YlOrRd,
                                )
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    return fig
+    choropleth_fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-@app.callback(
-    Output("graph-scatter", "figure"), 
-    Input("submit-geometry", "n_clicks"),
-    State("geometry-file", "value"),
-    State("gtfs-file", "value"))
-def display_scatter(n_clicks, geojson_file, gtfs_dir):
-    gdf = gpd.read_file(geojson_file)
-    gdf = gdf.to_crs(epsg=4258)
-    stops_file = gtfs_dir + '/stops.txt'
-    gtfs_df = pd.read_csv(stops_file)
-    gtfs_gdf = gpd.GeoDataFrame(gtfs_df, geometry=gpd.points_from_xy(gtfs_df.stop_lon, gtfs_df.stop_lat))
-
-    # Center of Wrocław
+        # Center of Wrocław
     point = (51.11046075118283, 17.03258604846201)
-    fig = go.Figure(px.scatter_mapbox(gdf, lat=[point[0]], lon=[point[1]]).update_layout(
-        mapbox={
-            "style": "open-street-map",
-            "zoom": 14,
-            "layers": [
-                {
-                    "source": json.loads(gdf.geometry.to_json()),
-                    "below": "traces",
-                    "type": "line",
-                    "color": "red",
-                    "line": {"width": 2.5},
-                },
-                                {
-                    "source": json.loads(gtfs_gdf.geometry.to_json()),
-                    "below": "traces",
-                    "type": "circle",
-                    "color": "blue",
-                    "circle": {"radius": 2.5},
-                }
-            ],
-        },
-        margin={"l": 0, "r": 0, "t": 0, "b": 0},
+
+    scatter_fig = go.Figure(px.scatter_mapbox(gdf, lat=[point[0]], lon=[point[1]]).update_layout(
+    mapbox={
+        "style": "open-street-map",
+        "zoom": 14,
+        "layers": [
+            {
+                "source": json.loads(gdf.geometry.to_json()),
+                "below": "traces",
+                "type": "line",
+                "color": "red",
+                "line": {"width": 2.5},
+            },
+                            {
+                "source": json.loads(gtfs_gdf.geometry.to_json()),
+                "below": "traces",
+                "type": "circle",
+                "color": "blue",
+                "circle": {"radius": 2.5},
+            }
+        ],
+    },
+    margin={"l": 0, "r": 0, "t": 0, "b": 0},
     ))
-    return fig 
+
+    return choropleth_fig, scatter_fig 
 
 
 # Run the app
