7a494519535015c7e9b5ffb43b044bcb504c652b
Michał Strzelczyk <817457+panmichal@users.noreply.github.com>
2024-12-11 12:42:09 +0100

    Extract map creation to separate functions

--- app.py
+++ app.py
@@ -78,42 +78,52 @@ app.layout = html.Div(
     Input("submit-geometry", "n_clicks"),
     State("geometry-file", "value"),
     State("gtfs-file", "value"))
-def display_chloropleth(n_clicks, geojson_file, gtfs_dir):
-    gdf = gpd.read_file(geojson_file)
-    gdf = gdf.to_crs(epsg=4258)
-    stops_file = gtfs_dir + '/stops.txt'
-    gtfs_df = pd.read_csv(stops_file)
-    gtfs_gdf = gpd.GeoDataFrame(gtfs_df, geometry=gpd.points_from_xy(gtfs_df.stop_lon, gtfs_df.stop_lat))
-    area_gdf = round(gdf.to_crs(epsg=32633).area / 10**6, 2)
+def display_maps(n_clicks, geojson_file, gtfs_dir):
+    if n_clicks == 0:
+        return {}, {}
+    else:
+        gdf = gpd.read_file(geojson_file)
+        gdf = gdf.to_crs(epsg=4258)
+        stops_file = gtfs_dir + '/stops.txt'
+        gtfs_df = pd.read_csv(stops_file)
+        gtfs_gdf = gpd.GeoDataFrame(gtfs_df, geometry=gpd.points_from_xy(gtfs_df.stop_lon, gtfs_df.stop_lat))
+        area_gdf = round(gdf.to_crs(epsg=32633).area / 10**6, 2)
 
-    gdf['area'] = area_gdf
+        gdf['area'] = area_gdf
 
-    gtfs_gdf['index_area'] = None
-    for index, row in gtfs_gdf.iterrows():
-        for index2, row2 in gdf.iterrows():
-            if row['geometry'].within(row2['geometry']):
-                gtfs_gdf.at[index, 'index_area'] = index2
-                break
+        gtfs_gdf['index_area'] = None
+        for index, row in gtfs_gdf.iterrows():
+            for index2, row2 in gdf.iterrows():
+                if row['geometry'].within(row2['geometry']):
+                    gtfs_gdf.at[index, 'index_area'] = index2
+                    break
 
-    # #count the number of stops in each area
-    count_by_area = gtfs_gdf.groupby('index_area').size().reset_index(name='count')
-    gdf = gdf.merge(count_by_area, left_index=True, right_on='index_area', how='left')
-    gdf['count'] = gdf['count'].fillna(0)
-    gdf['stops_density'] = gdf['area'] / gdf['count']
- 
-    choropleth_fig = px.choropleth_mapbox(gdf, geojson=gdf.geometry, locations=gdf.index, color="stops_density",
-                               hover_data=["NAZWAOSIED", "count", "area", "stops_density"],
-                               mapbox_style="carto-positron",
-                               zoom=11, center = {"lat": 51.11046075118283, "lon": 17.03258604846201},
-                               opacity=0.5,
-                               color_continuous_scale=px.colors.sequential.YlOrRd,
-                               )
-    choropleth_fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        # #count the number of stops in each area
+        count_by_area = gtfs_gdf.groupby('index_area').size().reset_index(name='count')
+        gdf = gdf.merge(count_by_area, left_index=True, right_on='index_area', how='left')
+        gdf['count'] = gdf['count'].fillna(0)
+        gdf['stops_density'] = gdf['area'] / gdf['count']
 
         # Center of Wrocław
-    point = (51.11046075118283, 17.03258604846201)
+        center = (51.11046075118283, 17.03258604846201)
+        area_name_field = "NAZWAOSIED"
 
-    scatter_fig = go.Figure(px.scatter_mapbox(gdf, lat=[point[0]], lon=[point[1]]).update_layout(
+        return chloropleth_map(gdf, center, area_name_field), scatter_map(gdf, gtfs_gdf, center)
+    
+def chloropleth_map(gdf, center, area_name_field):
+    choropleth_fig = px.choropleth_mapbox(gdf, geojson=gdf.geometry, locations=gdf.index, color="stops_density",
+                        hover_data=[area_name_field, "count", "area", "stops_density"],
+                        mapbox_style="carto-positron",
+                        zoom=11, center = {"lat": center[0], "lon": center[1]},
+                        opacity=0.5,
+                        color_continuous_scale=px.colors.sequential.YlOrRd,
+                        )
+    choropleth_fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    return choropleth_fig
+
+def scatter_map(gdf, gtfs_gdf, center):
+    scatter_fig = go.Figure(px.scatter_mapbox(gdf, lat=[center[0]], lon=[center[1]]).update_layout(
     mapbox={
         "style": "open-street-map",
         "zoom": 14,
@@ -136,8 +146,9 @@ def display_chloropleth(n_clicks, geojson_file, gtfs_dir):
     },
     margin={"l": 0, "r": 0, "t": 0, "b": 0},
     ))
+            
+    return scatter_fig
 
-    return choropleth_fig, scatter_fig 
 
 
 # Run the app
